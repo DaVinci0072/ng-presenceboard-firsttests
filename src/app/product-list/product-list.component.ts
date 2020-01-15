6d6bc7cd8eca8e5777c0df28a9b7a322571fccea
DaVinci0072 <ryan@ryanbelanger.com>
2020-01-14 20:25:06 -0500

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -14,6 +14,14 @@ export class ProductListComponent {
 
   constructor(private modalService: NgbModal) {}
 
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
   share() {
     window.alert('The product has been shared!');
   }
