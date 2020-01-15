155c0afdafde8e0a3c8de96967a77399031178c7
DaVinci0072 <ryan@ryanbelanger.com>
2020-01-14 20:38:03 -0500

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -16,9 +16,9 @@ export class ProductListComponent {
 
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
+      this.closeResult = 'Closed with: ${result}';
     }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      this.closeResult = 'Dismissed ${this.getDismissReason(reason)}';
     });
   }
 
@@ -28,6 +28,16 @@ export class ProductListComponent {
 
   onNotify() {
     window.alert('You will be notified when the product goes on sale');
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  'with: ${reason}';
+    }
   }
 }
 
